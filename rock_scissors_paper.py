188cd1fa2535a8b4afd161f543cc51f3d3ea7217
Danny Lu <danny.lu075@gmail.com>
2019-09-11 09:27:27 -0400

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -1,10 +1,26 @@
 """
 Rock, Scissors, Paper
 
-Group members:
+Group members: Nathan, Danny, 
 """
+import pygame
+import random
 
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+display_width = 800
+display_height = 600
+pygame.init()
+screen = pygame.display.set_mode([display_width, display_height])
+pygame.display.set_caption("Rock, Scissors, Paper")
+clock = pygame.time.Clock()
+game = True
 
-def some_function():
-    return True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
 
+pygame.quit()
+quit()
