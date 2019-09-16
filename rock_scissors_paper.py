9b186f7e286ea581dfa94c16bd581ef8b41ffd21
Danny Lu <danny.lu075@gmail.com>
2019-09-16 10:16:48 -0400

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -4,42 +4,58 @@ Rock, Scissors, Paper
 Group members: Nathan, Danny, 
 """
 import pygame
-import random
+from random import randint
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 display_width = 800
 display_height = 600
-
 rock = pygame.transform.scale((pygame.image.load('rock.jpg')), (150, 150))
 paper = pygame.transform.scale((pygame.image.load('paper.png')), (150, 150))
 scissors = pygame.transform.scale((pygame.image.load('scissors.jpg')), (150, 150))
 
 pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont('Comic Sans MS', 30)
 screen = pygame.display.set_mode([display_width, display_height])
 screen.fill(WHITE)
 pygame.display.set_caption("Rock, Scissors, Paper")
 clock = pygame.time.Clock()
 
-game = True
+choices = ["Rock", "Paper", "Scissors"]
 
+page = 0
+game = True
+title = font.render('Rock Paper Scissors', False, (0, 0, 0))
 
 def draw():
     screen.blit(rock, (50, 300))
     screen.blit(paper, (325, 300))
     screen.blit(scissors, (625, 300))
+    screen.blit(title, (250, 50))
 
+def page_1():
+    pass
 
 while game:
+    computer_choice = choices[randint(0, 2)]
     for event in pygame.event.get():
         draw()
         x, y = pygame.mouse.get_pos()
-        
+        if 50 <= x <= 200 and 350 >= y <= 200:
+            if computer_choice == "Rock":
+                page = 1
+            elif computer_choice == "Paper":
+                page = 2
+            elif computer_choice == "Scissors":
+                page = 3
+
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
     pygame.display.flip()
+    
 pygame.quit()
 quit()
